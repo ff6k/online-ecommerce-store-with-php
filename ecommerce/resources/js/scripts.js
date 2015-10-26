f050fa29196e421cbe5aaa5f3ed49f6083874b86
coolerwind <brzden32@gmail.com>
2015-10-26 17:02:37 +0100

    Slider amb fletxes
    - El slider conté fletxes cap als costats que permeten desplaçar-se.

--- ecommerce/resources/js/scripts.js
+++ ecommerce/resources/js/scripts.js
@@ -36,6 +36,48 @@ function nextImageSlider(){
     setTimeout(nextImageSlider, 5000);
 }
 
+function sliderLeft(){
+
+    var imageBanner1 = document.getElementById("image_banner_1");
+    var imageBanner2 = document.getElementById("image_banner_2");
+    var imageBanner3 = document.getElementById("image_banner_3");
+
+    if(imageBanner1 != null){
+        imageBanner1.src = "resources/img/banner3.png";
+        imageBanner1.id = "image_banner_3";
+        imageBanner1.href ="http://www.google.es"
+    }else if(imageBanner2 != null){
+        imageBanner2.src = "resources/img/banner1.png";
+        imageBanner2.id = "image_banner_1";
+        imageBanner2.href ="http://www.google.es"
+    }else if(imageBanner3 != null){
+        imageBanner3.src = "resources/img/banner2.png";
+        imageBanner3.id = "image_banner_2";
+        imageBanner3.href ="http://www.google.es"
+    }
+}
+
+function sliderRight(){
+
+    var imageBanner1 = document.getElementById("image_banner_1");
+    var imageBanner2 = document.getElementById("image_banner_2");
+    var imageBanner3 = document.getElementById("image_banner_3");
+
+    if(imageBanner1 != null){
+        imageBanner1.src = "resources/img/banner2.png";
+        imageBanner1.id = "image_banner_2";
+        imageBanner1.href ="http://www.google.es"
+    }else if(imageBanner2 != null){
+        imageBanner2.src = "resources/img/banner3.png";
+        imageBanner2.id = "image_banner_3";
+        imageBanner2.href ="http://www.google.es"
+    }else if(imageBanner3 != null){
+        imageBanner3.src = "resources/img/banner1.png";
+        imageBanner3.id = "image_banner_1";
+        imageBanner3.href ="http://www.google.es"
+    }
+}
+
 function enter(e){
     if(e.keyCode == 13){
         buscar();
